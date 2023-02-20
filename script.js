62378e53e04530b5577b8e7ea3ddaae7f3bde5de
Parv Gupta <Parvg555@gmail.com>
2023-02-21 00:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const searchxyz = document.querySelector('.search')
+const btnxyz = document.querySelector(".btn")
+const inputxyz = document.querySelector(".input")
+
+btnxyz.addEventListener("click",function(){
+    searchxyz.classList.toggle("active")
+    inputxyz.focus()
+})
